c207bfd465d7ec4379c138535521e581f624113f
HeinDiez <heindiez@gmail.com>
2022-10-17 10:47:16 +0200

    update

--- serverless/handler.js
+++ serverless/handler.js
@@ -12,20 +12,20 @@ module.exports.calculate = (event, context, callback) => {
             // assuming 1 ton is equal to 1000
             return ton * 1000;
         };
-        const calculateEV = (data) => {
+        const calculateEnergyValue = (data) => {
             const variables = {
-                energyCost: 13500000,
+                energyCost: 13500000, //1.35*10^7 per consumedEnergy
                 fuelCost: 15
             };
-            const FM = convTONtoKG(data.rocket.rocket.second_stage.fuel_amount_tons);
-            const RM = data.rocket.rocket.mass.kg;
+            const fuelMass = convTONtoKG(data.rocket.rocket.second_stage.fuel_amount_tons);
+            const rocketMass = data.rocket.rocket.mass.kg;
 
-            const CE = (RM + FM) * variables.fuelCost;
-            const EV = variables.energyCost * CE;
+            const consumedEnergy = (rocketMass + fuelMass) * variables.fuelCost;
+            const energyValue = variables.energyCost * consumedEnergy;
 
-            return { ...data, energyValue: EV };
+            return { ...data, energyValue };
         };
-        const data = event.data.map((rocket) => calculateEV(rocket));
+        const data = event.data.map((rocket) => calculateEnergyValue(rocket));
         callback(null, data);
     } catch (error) {
         callback(true, error);
